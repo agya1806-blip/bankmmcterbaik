9b1e3c9a2a364b85183e2ad39f7591da9bf252c3
Agya <agya1806@blip.dev>
2026-07-19 15:01:34 +0700

    chore: add deployment config (Vercel, Docker, standalone output)

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "standalone",
   experimental: {
     scrollRestoration: true,
   },
+  poweredByHeader: false,
 };
 
 module.exports = nextConfig;
